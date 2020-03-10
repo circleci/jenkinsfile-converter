27faf0d9152c7a7d41a8dceed051f6d66df78287
Makoto Mizukami <makoto@circleci.com>
2020-03-10 18:32:22 +0900

    Experimental implementation of classes

--- util/circleci.js
+++ util/circleci.js
@@ -2,14 +2,11 @@ const { writeExecutors } = require('./configExecutors.js');
 const { writeWorkflows } = require('./configWorkflows.js');
 const { writeJobs } = require('./configJobs.js');
 const { writeComments } = require('./configComments.js');
+const yaml = require('js-yaml');
 
 const createConfig = (workflowObj) => {
-  let config = ''
-  config += writeExecutors(workflowObj);
-  config += writeWorkflows(workflowObj);
-  config += writeJobs(workflowObj);
-  config += writeComments(workflowObj);
-  return config;
+  console.log(workflowObj);
+  return yaml.safeDump(workflowObj, { skipInvalid: true });
 }
 
 module.exports = { createConfig };
