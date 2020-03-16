598d23913c603914dc626479d4ee08b4fd7125c5
Makoto Mizukami <makoto@circleci.com>
2020-03-16 16:46:13 +0900

    Enhanced test for CircleConfig

--- test/modules/unit/CircleConfig.js
+++ test/modules/unit/CircleConfig.js
@@ -7,16 +7,35 @@ describe('CircleConfig', () => {
     const obj = new CircleConfig(2.1);
     const commentString = 'My comments!';
 
-    obj.comments.push(commentString);
-
     describe('constructor', () => {
         it('should have mark version as 2.1', () => {
             assert(obj.version === 2.1);
         });
+
+        it('should have an empty job list', () => {
+            expect(obj.jobs).to.be.a('object');
+            assert(Object.getOwnPropertyNames(obj.jobs).length === 0);
+        });
+
+        it('should have an initialized workflows stanza', () => {
+            expect(obj.workflows).to.be.a('object');
+            assert(obj.workflows.version === 2);
+            assert(Object.getOwnPropertyNames(obj.workflows).length === 1);
+        });
+
+        it('should have an empty comments', () => {
+            expect(obj.comments).to.be.a('array');
+            assert(obj.comments.length === 0);
+        });
     });
 
     describe('toYAML', () => {
-        const yaml = obj.toYAML();
+        let yaml = '';
+
+        before(() => {
+            obj.comments.push(commentString);
+            yaml = obj.toYAML();
+        });
 
         it('should rerutn string', () => {
             expect(yaml).to.be.a('string');
