778bf5e1c24e4b24c6d1e0c4eccc11659148340b
Jaryt <jarytbustard@Gmail.com>
2020-04-01 00:29:03 -0400

    Added Jenkins-JSON converter base.

--- index.js
+++ index.js
@@ -1,39 +1,25 @@
-// This is a proof of concept of converting declarative Jenkinsfiles to CircleCI 2.0 config
-
-// The intention of this script in its current state is not to be the interface that a user will interact with, but just a POC of the conversion from Jenkinsfiles to CCI config.
-
 const fs = require('fs');
-const path = require('path');
+const request = require('request');
+const openFile = (path) => fs.readFileSync(path, { encoding: 'utf8' });
 
-const { openFile, verifyValid } = require('./util/file.js');
-const { parseJenkinsfile } = require('./util/jenkins.js');
+const inputPath = process.argv[2];
+const jenkinsin = openFile(inputPath);
 
-// TODO: Groovy library to interact with Jenkinsfiles?
-// TODO: YAML Library to handle/validate output?
+var user = process.env.JENKINS_USER;
+var pass = process.env.JENKINS_PASS;
 
-// TODO: Pair Jenkinsfiles syntax key with CCI syntax key
+request
+  .post(
+    'http://ec2-18-233-208-176.compute-1.amazonaws.com:8080/pipeline-model-converter/toJson',
+    function(error, response, body) {
+      if (response.statusCode == 200) {
+        var jenkins_json = JSON.parse(body)['data']['json'];
 
-{
-  const inputPath = process.argv[2];
-  const outputPath = process.argv[3] || 'config.yml';
-  const jenkinsfile = openFile(inputPath);
-
-  if (!verifyValid(jenkinsfile)) {
-    //TODO: return error and change exit
-    console.error(
-      'Invalid configuration. This tool only supports Jenkinsfiles using declarative pipelines.'
-    );
-  }
-
-  {
-    const circleConfig = parseJenkinsfile(jenkinsfile);
-    let circleYAML = circleConfig.toYAML();
-
-    // Hacking - Advisory for executors is inserted as
-    // We remove the property definition here and put the advisory text as comments.
-    circleYAML = circleYAML.replace(/^\s*advisory_for_users: \|-\s*\n\n/m, '');
-
-    fs.writeFileSync(path.join(__dirname, outputPath), circleYAML);
-    console.log('file saved!');
-  }
-}
+        console.log(JSON.stringify(jenkins_json, null, 4));
+      } else if (error != null) {
+        console.error(error);
+      }
+    }
+  )
+  .auth(user, pass)
+  .form({ jenkinsfile: jenkinsin.toString() });
