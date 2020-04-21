424862f37920e8c0f0b1464e1f3fc45a481a858b
Eugene C <eugene@circleci.com>
2020-04-21 14:14:33 +0900

    Initial commit for JF steps -> CCI Model adapter Write tests for JF -> CCI adapter

---
+++ model/CircleStep.js
@@ -0,0 +1,13 @@
+class CircleStep {
+  comment;
+  command;
+  environment;
+  working_directory;
+  name;
+  shell;
+  background;
+  no_output_timeout;
+  when;
+}
+
+module.exports = { CircleStep };
