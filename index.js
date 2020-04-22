25c1371faee0000523d53baf57195f2058588fb4
Makoto Mizukami <makoto@circleci.com>
2020-04-22 11:36:21 +0900

    Rewrite API call to Jenkins with the native https module and Promise

--- index.js
+++ index.js
@@ -1,26 +1,77 @@
 const fs = require('fs');
-const request = require('request');
-const openFile = (path) => fs.readFileSync(path, { encoding: 'utf8' });
+const https = require('https');
+const querystring = require('querystring');
+
 const { map } = require('./mapping/mapper.js');
 
-const inputPath = process.argv[2];
-const jenkinsin = openFile(inputPath);
+{
+  const groovyToJSONHTTPSCB = (resolve, reject, res) => {
+    const dataChunks = [];
 
-var user = process.env.JENKINS_USER;
-var pass = process.env.JENKINS_PASS;
+    res.on('data', (data) => {
+      dataChunks.push(data);
+    });
 
-request
-  .post(
-    'http://ec2-18-233-208-176.compute-1.amazonaws.com:8080/pipeline-model-converter/toJson',
-    function(error, response, body) {
-      if (response.statusCode == 200) {
-        var jenkins_json = JSON.parse(body)['data']['json'];
+    res.on('end', () => {
+      const resBodyStr = Buffer.concat(dataChunks).toString();
 
-        map(jenkins_json);
-      } else if (error != null) {
-        console.error(error);
+      if (res.statusCode === 200) {
+        resolve(resBodyStr);
+      } else {
+        reject(resBodyStr);
       }
+    });
+
+    res.on('error', (err) => {
+      reject(err);
+    });
+  };
+
+  const groovyToJSONRunner = (groovyStr, resolve, reject) => {
+    try {
+      const bodyData = querystring.stringify({ jenkinsfile: groovyStr });
+      const req = https.request(
+        'https://jenkinsto.cc/i',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': bodyData.length
+          }
+        },
+        groovyToJSONHTTPSCB.bind(null, resolve, reject)
+      );
+
+      req.write(bodyData);
+      req.end();
+    } catch (err) {
+      reject(err);
     }
-  )
-  .auth(user, pass)
-  .form({ jenkinsfile: jenkinsin.toString() });
+  };
+
+  const groovyToJSONPromise = (groovyStr) => {
+    return new Promise(groovyToJSONRunner.bind(null, groovyStr));
+  };
+
+  // Main from here
+  (async () => {
+    // TODO: Avoid nesting try-catch
+    try {
+      const inputPath = process.argv[2];
+      const jenkinsJSON = await groovyToJSONPromise(fs.readFileSync(inputPath, 'utf8'));
+
+      try {
+        const jenkinsObj = JSON.parse(jenkinsJSON).data.json;
+        const circleConfig = map(jenkinsObj);
+
+        console.log(circleConfig.toYAML());
+      } catch (err) {
+        console.error(err);
+        console.error('Error in conversion');
+      }
+    } catch (err) {
+      console.error(err);
+      console.error('Error in Jenkins');
+    }
+  })();
+}
