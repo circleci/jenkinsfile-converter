82b24c44393aa8aaecdb47ee6e43d6e42d97d943
Makoto Mizukami <makoto@circleci.com>
2020-05-09 04:13:44 +0900

    Output to file if specified

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+const fs = require('fs');
+
 (async () => {
-  console.log(
-    await require('./main.js').jenkinsToCCI(require('fs').readFileSync(process.argv[2], 'utf8'))
-  );
+  const res = await require('./main.js').jenkinsToCCI(fs.readFileSync(process.argv[2], 'utf8'));
+
+  console.log(res);
+
+  if (process.argv[3]) {
+    fs.writeFileSync(process.argv[3], res);
+  }
 })();
