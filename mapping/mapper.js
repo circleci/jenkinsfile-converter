a935945e7715316a82e54b67b286dcf6e70261c8
Makoto Mizukami <makoto@circleci.com>
2020-10-14 16:05:32 +0900

    Linter + Prettier

--- mapping/mapper.js
+++ mapping/mapper.js
@@ -73,7 +73,8 @@ const mapStages = (stages, mapEnvironment, config) => {
 };
 
 const dockerImagesForJob = (stage) => {
-  if (stage.agent && stage.agent.type === 'docker' && stage.agent.arguments) {  // Agent is Docker. Find the argument with `key` having `image`
+  if (stage.agent && stage.agent.type === 'docker' && stage.agent.arguments) {
+    // Agent is Docker. Find the argument with `key` having `image`
     const ret = [];
 
     stage.agent.arguments.forEach((argument) => {
@@ -83,7 +84,8 @@ const dockerImagesForJob = (stage) => {
     });
 
     return ret;
-  } else {  // Fallback
+  } else {
+    // Fallback
     // goes 2 days back to make sure monthly snapshot has had time to be created (2nd of month)
     let curDate = new Date(Date.now() - 172800000);
     let curMonth = (curDate.getMonth() < 9 ? '0' : '') + (curDate.getMonth() + 1);
